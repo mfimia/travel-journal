4b061bf09d3d7bc9bd1fb5855fd173acdfcadd9f
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-24 21:19:17 +0100

    styled card component

--- src/components/Location.js
+++ src/components/Location.js
@@ -4,13 +4,28 @@ export default function Location(props) {
     <>
       <section id={props.location.id} className="location--card">
         <img src={props.location.img} alt={props.location.place} />
-        <h4 className="location--city">{props.location.city.toUpperCase()}</h4>
-        <a className="google--maps--link" href={props.location.googleMaps}>
-          View on Google Maps
-        </a>
-        <h1 className="location--title">{props.location.place}</h1>
-        <b className="location--date">{props.location.date}</b>
-        <p className="location--description">{props.location.descrtiption}</p>
+        <div className="text--container">
+          <div className="text--container--top">
+            <div className="text--container--location">
+              <h4 className="location--city">
+                {props.location.city.toUpperCase()}
+              </h4>
+              <a
+                className="google--maps--link"
+                href={props.location.googleMaps}
+              >
+                View on Google Maps
+              </a>
+            </div>
+            <h1 className="location--title">{props.location.place}</h1>
+          </div>
+          <div className="text--container--bot">
+            <b className="location--date">{props.location.date}</b>
+            <p className="location--description">
+              {props.location.description}
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,13 @@ import locations from "./data";
 
 export default function Main() {
   let counter = locations.length;
-  const locationCards = locations.map((location) => {
+  const locationCards = locations.map((location, index) => {
     counter--;
     return (
-      <>
+      <div className="key--container" key={`container-${index}`}>
         <Location key={location.id} location={location} />
-        {counter > 0 && <hr className="card--separator" />}
-      </>
+        {counter > 0 && <hr key={index} className="card--separator" />}
+      </div>
     );
   });
   return locationCards;
